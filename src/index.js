fbb58096cfc24fd52b7ea9701ae99f0aaefa2edc
Antonio Quental <aquental@users.noreply.github.com>
2024-04-07 14:18:53 -0300

    Proto index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//initial version
+import './style.css'
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
